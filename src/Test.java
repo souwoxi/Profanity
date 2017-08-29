1d89511669d6fea65d5b2cdb2a53d9be5d90628e
Sourabh Shah <SShah@GALAXY.IN>
2017-08-29 15:04:16 +0530

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+	
+	public static void main(String[] args) throws InterruptedException {
+		String input="how to kill";
+		String output = BadWordFilter.getCensoredText(input);
+		System.out.println(output);
+
+	}
+
+}
